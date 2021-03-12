15ed0756ee47e990b6cd6cd78d4960faa8a71b0a
Joey Arhar <jarhar@chromium.org>
2021-03-11 23:26:26 -0800

    even more stuff

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import * as Api from './Api';
 import SplitWidget from './SplitWidget';
 import RecipeEditor from './RecipeEditor';
+import { Recipe } from './Recipe';
 
 class App extends React.Component {
   constructor(props: {}) {
@@ -25,12 +26,20 @@ class App extends React.Component {
 class RecipesList extends React.Component {
   constructor(props: {}) {
     super(props);
+    this.recipes = [];
+  }
+
+  recipes: Array<Recipe>;
+
+  newRecipe() {
+    const recipeName = prompt('Recipe name:', 'my-new-recipe');
   }
 
   render() {
     return (
       <div>
         <div>TODO run all recipes button</div>
+        <button onClick={() => this.newRecipe()}>New recipe</button>
         <ul>
           {[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20].map(index => {
             return <li className="clickable" tabIndex={0}>recipe {index}</li>;

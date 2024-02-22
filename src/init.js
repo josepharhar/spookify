e445a17873339b001e3886cfbe47a6993ef8b57a
Joey Arhar <josepharhar@gmail.com>
2024-02-21 22:39:49 -0800

    going to try running now

--- src/init.js
+++ src/init.js
@@ -1,16 +1,48 @@
 import { SpotifyApi } from '/node_modules/@spotify/web-api-ts-sdk/dist/mjs/index.js';
+import 'error-popover.js';
 
 console.log('spotifyapi: ', SpotifyApi);
 window.spotifyapi = SpotifyApi;
 
-class InitElement extends HTMLElement {
+class SpookifyInit extends HTMLElement {
   constructor() {
     super();
   }
 
   connectedCallback() {
-    this.innerHTML = 
+    // TODO consider putting this in a shadowroot instead with adoptedStyleSheets?
+    this.innerHTML = `
+      <dialog>
+        <label for=clientid>Client ID</label>
+        <input id=clientid>
+        <br>
+        <label for=clientsecret>Client Secret</label>
+        <input id=clientsecret>
+        <br>
+        <button>login to spotify</button>
+      </dialog>
+    `;
+    const clientidElement = this.querySelector('#clientid');
+    const clientsecretElement = this.querySelector('#clientsecret');
+    const dialog = this.querySelector('dialog');
+    const button = this.querySelector('button');
+
+    button.onclick = () => {
+      // TODO does this throw an exception or something?
+      try {
+        window.spotifyApi = SpotifyApi.withClientCredentials(
+          clientidElement.value,
+          clientsecretElement.value);
+      } catch (error) {
+        const errorPopover = document.createElement('spookify-error-popover');
+        errorPopover.message = error;
+        this.appendChild(errorPopover);
+      }
+      dialog.close();
+    };
+
+    dialog.showModal();
   }
 };
 
-customElements.define('spookify-init
+customElements.define('spookify-init', SpookifyInit);

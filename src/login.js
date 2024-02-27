fbdbc27db1c70f0b9138c4b087758f8a73458724
Joey Arhar <josepharhar@gmail.com>
2024-02-26 18:57:03 -0800

    started on oauth

--- src/login.js
+++ src/login.js
@@ -10,27 +10,42 @@ class SpookifyLogin extends HTMLElement {
   connectedCallback() {
     // TODO consider putting this in a shadowroot instead with adoptedStyleSheets?
     // TODO consider using a form element for submission keyboard behavior
+    const scopes = [
+      'playlist-read-private',
+      'playlist-read-collaborative',
+      'playlist-modify-private',
+      'playlist-modify-public',
+      'user-library-modify',
+      'user-library-read',
+      'user-read-email',
+      'user-read-private'
+    ];
+    const scopesString = scopes.join('%20');
+
+    const actionUrl = new URLSearchParams();
+    actionUrl.set('response_type', 'code');
+    actionUrl.set('redirect_uri', window.location.href);
+    actionUrl.set('scope', scopesString);
+
     this.innerHTML = `
       <dialog>
-        <label for=clientid>Client ID</label>
-        <input id=clientid>
-        <br>
-        <label for=clientsecret>Client Secret</label>
-        <input id=clientsecret>
-        <br>
-        <button>login to spotify</button>
+        <form action="${actionUrl.toString()}">
+          <label for=clientid>Client ID</label>
+          <input id=clientid name=client_id>
+          <br>
+          <button>login to spotify</button>
+        </form>
       </dialog>
     `;
     const clientidElement = this.querySelector('#clientid');
-    const clientsecretElement = this.querySelector('#clientsecret');
     const dialog = this.querySelector('dialog');
     const button = this.querySelector('button');
 
-    clientidElement.value = window.localStorage.clientid;
-    clientsecretElement.value = window.localStorage.clientsecret;
+    clientidElement.value = window.localStorage.clientid || '';
 
     button.onclick = () => {
       // TODO does this throw an exception or something?
+      const secret = 
       try {
         window.spotifyApi = SpotifyApi.withClientCredentials(
           clientidElement.value,

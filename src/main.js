28d632c59c4d6cfadbf9f5d78f2d95328536af8f
Joey Arhar <josepharhar@gmail.com>
2025-03-16 10:09:07 -0700

    doing things

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 const code = new URLSearchParams(window.location.search).get('code');
+const playlistsNum = document.getElementById('playlistsnum');
+const playlistsLastUpdated = document.getElementById('playlistslastupdated');
+const playlistsUpdate = document.getElementById('playlistsupdate');
+const likedSongsNum = document.getElementById('likedsongsnum');
+const likedSongsLastUpdated = document.getElementById('likedsongslastupdated');
+const likedSongsUpdate = document.getElementById('likedsongsupdate');
 
 function dialogError(html) {
   const dialog = document.createElement('dialog');
-  dialog.innerHTML = html;
+
+  const restartButton = document.createElement('button');
+  restartButton.textContent = 'log in again';
+  dialog.appendChild(restartButton);
+  restartButton.addEventListener('click', () => {
+    window.location.href = '/';
+  });
+
+  const output = document.createElement('div');
+  output.innerHTML = html;
+  dialog.appendChild(output);
+
   document.body.appendChild(dialog);
   dialog.showModal();
 }
@@ -38,57 +55,11 @@ function log(str) {
   console.log(str);
   const div = document.createElement('div');
   div.textContent = str;
-  document.getElementById('output').appendChild(div);
+  document.getElementById('log').appendChild(div);
 }
 
-(async () => {
-  if (!code) {
-    dialogError('code query parameter is missing');
-    return;
-  }
-
-  const codeVerifier = window.localStorage.codeVerifier;
-  if (!codeVerifier) {
-    dialogError('codeVerifier is missing from localStorage');
-    return;
-  }
-
-  log('code: ' + code);
-  log('going to get authorization code...');
-
-  try {
-    const payload = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: new URLSearchParams({
-        client_id: window.localStorage.clientid,
-        grant_type: 'authorization_code',
-        code,
-        redirect_uri: window.location.origin + '/main.html',
-        code_verifier: codeVerifier,
-      }),
-    };
-    // TODO add error handling and logging here...?
-    const body = await fetch('https://accounts.spotify.com/api/token', payload);
-    const response = await body.json();
-    console.log('got response for authorization code:', response);
-    if (!response.access_token) {
-      dialogError('no access token in response: <pre>' + JSON.stringify(response, null, 2));
-      return;
-    }
-    window.localStorage.accessToken = response.access_token;
-  } catch (error) {
-    dialogError('got error while getting access token: ' + error);
-    return;
-  }
-
-  log('access_token: ' + window.localStorage.accessToken);
-
-  log('going to fetch /me to window.meResult...');
-  window.meResult = await fetchWebApi('me', 'GET');
-
+// Downloads playlists from spotify and stores them in idb
+async function downloadPlaylists() {
   log('going to fetch /me/playlists to window.mePlaylistsResult...');
   let playlists = [];
   let nextUrl = 'https://api.spotify.com/v1/me/playlists?limit=50';
@@ -104,15 +75,96 @@ function log(str) {
     if (!response.ok) {
       // TODO handle rate limiting here
       dialogError('got bad response: ' + response.status);
-      console.log('got bad response: ', response);
+      log('got bad response: ', response);
       return;
     }
     const json = await response.json();
     playlists = playlists.concat(json.items);
     nextUrl = json.next;
   }
-  idbKeyval.set('playlists', playlists);
-  idbKeyval.set('playlists-timestamp', new Date().toString());
+  await idbKeyval.set('playlists', playlists);
+  await idbKeyval.set('playlists-timestamp', new Date().toString());
+}
+
+// Sets access token to localStorage.accessToken. Returns true if there was an
+// issue.
+async function downloadAccessToken() {
+  try {
+    const payload = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        client_id: window.localStorage.clientid,
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: window.location.origin + '/main.html',
+        code_verifier: window.localStorage.codeVerifier,
+      }),
+    };
+    const body = await fetch('https://accounts.spotify.com/api/token', payload);
+    const response = await body.json();
+    console.log('got response for authorization code:', response);
+    if (!response.access_token) {
+      dialogError('no access token in response: <pre>' + JSON.stringify(response, null, 2));
+      return true;
+    }
+    window.localStorage.accessToken = response.access_token;
+  } catch (error) {
+    dialogError('got error while getting access token: ' + error);
+    return true;
+  }
+  return false;
+}
+
+async function updatePlaylistsText() {
+  const cachedPlaylists = await idbKeyval.get('playlists');
+  const cachedPlaylistsTimestamp = await idbKeyval.get('playlists-timestamp');
+  playlistsNum.textContent = cachedPlaylists ? cachedPlaylists.length : 'none';
+  playlistsLastUpdated.textContent = cachedPlaylistsTimestamp ? cachedPlaylistsTimestamp : 'never';
+}
+
+async function updateLikedSongsText() {
+  const cachedLikedSongs = await idbKeyval.get('likedsongs');
+  const cachedLikedSongsTimestamp = await idbKeyval.get('likedsongs-timestamp');
+  likedSongsNum.textContent = cachedLikedSongs ? cachedLikedSongs.length : 'none';
+  likedSongsLastUpdated.textContent = cachedLikedSongsTimestamp ? cachedLikedSongsTimestamp : 'never';
+}
+
+(async () => {
+  if (!code) {
+    dialogError('code query parameter is missing');
+    return;
+  }
+
+  if (!window.localStorage.codeVerifier) {
+    dialogError('codeVerifier is missing from localStorage');
+    return;
+  }
+
+  await updatePlaylistsText();
+  playlistsUpdate.addEventListener('click', async () => {
+    await downloadPlaylists();
+    await updatePlaylistsText();
+  });
+
+  await updateLikedSongsText();
+  likedSongsUpdate.addEventListener('click', async () => {
+    await downloadLikedSongs();
+    await updateLikedSongsText();
+  });
+
+  log('code: ' + code);
+  log('going to get authorization code...');
+  if (await downloadAccessToken()) {
+    log('failed to get access token. aborting.');
+    return;
+  }
+  log('access_token: ' + window.localStorage.accessToken);
+
+  /*log('going to fetch /me to window.meResult...');
+  window.meResult = await fetchWebApi('me', 'GET');*/
 })();
 
 // TODO use snapshot_id to cache playlists: https://developer.spotify.com/documentation/web-api/concepts/rate-limits#:~:text=set%20of%20objects.-,Use%20the%20snapshot_id,-Playlist%20APIs%20expose
